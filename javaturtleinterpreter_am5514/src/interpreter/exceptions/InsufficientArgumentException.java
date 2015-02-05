c45929991752bd8254bf14f662893296eed020af
Ayman <aymanm95@gmail.com>
2015-02-05 02:59:34 +0000

    More changes @3:00 AM

---
+++ javaturtleinterpreter_am5514/src/interpreter/exceptions/InsufficientArgumentException.java
@@ -0,0 +1,9 @@
+package interpreter.exceptions;
+
+public class InsufficientArgumentException extends Exception {
+  
+  public InsufficientArgumentException(String message){
+    super(message);
+}
+
+}
