3387e87f2a09c6ff3ff2078264a24b180e0d7fad
Ayman Moussa <aymanm95@gmail.com>
2015-05-24 22:56:52 +0100

    master commit

--- C/GameBoyDisassembler/GameBoyDisassembler/main.c
+++ C/GameBoyDisassembler/GameBoyDisassembler/main.c
@@ -8,7 +8,7 @@ struct opcode** opcodeMapCB;
 
 #pragma warning (disable : 4996)
 
-typedef unsigned char byte; //Should be globally defined
+typedef unsigned char byte; //Should be globally defined 
 
 //GameBoy begins executing instructions at 0x100 in memory
 #define INSTRUCTION_EXECUTION_ADDRESS 0x100
